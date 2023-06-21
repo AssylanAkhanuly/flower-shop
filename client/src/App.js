f093353b6bbf14935a203e7c02d4c09b13a86c9c
Assylan Akhanuly <assylan.akhanuly@gmail.com>
2023-06-21 09:13:53 +0600

    loading animation

--- client/src/App.js
+++ client/src/App.js
@@ -68,7 +68,7 @@ function App() {
 
   return (
     <div className="App">
-      <Suspense fallback={<Loader />}>
+      {/* <Suspense fallback={<Loader />}>
         <Canvas
           ref={canvasRef}
           style={{ background: "#e6d1b6" }}
@@ -122,7 +122,8 @@ function App() {
             position={[-9, 4.2, 22.5]}
           />
         </Canvas>
-      </Suspense>
+      </Suspense> */}
+      <Loader/>
       {started && progress === 100 && (
         <>
           <SocialList />
--- client/src/components/Loader/Loader.jsx
+++ client/src/components/Loader/Loader.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./loader.css";
+import { gsap } from "gsap";
 function Loader() {
+   const [repeat, setRepeat] = useState(false)
+  useEffect(() => {
+    gsap.fromTo(".bubble", {
+      top: "100%",
+    },
+     {
+      top: "-30%",
+      duration: 25,
+      onComplete: () => {
+        setRepeat(!repeat);
+      }
+    })
+  }, [repeat])
   return (
     <div className="loading-page">
       <h2 className="welcome-title">Welcome to</h2>
@@ -12,8 +26,42 @@ function Loader() {
       />
       <h1 className="welcome-content">Bunny</h1>
       <h1 className="welcome-content">Flower Shop </h1>
-
       <div className="loading-circle"></div>
+      <div className="bubble">
+        <span></span> 
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="bubble first">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="bubble second">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="bubble third">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="bubble forth">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>  
     </div>
   );
 }
